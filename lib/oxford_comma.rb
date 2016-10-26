2b11c105fc9207ce76b71b30ab926852cf453d07
Jennifer Wong <wongjenn@yahoo.com>
2016-10-26 17:58:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,17 @@
 def oxford_comma(array)
   num = array.length
 
+  case num
+  when 1
+    array.join("")
+  when 2
+    array.join(" and ")
+  else
+    last_word = array.pop.to_s
+    array.join(", ") + ", and #{last_word}"
+  end
+  
+=begin
   if num == 1
     array.join("")
   elsif num == 2
@@ -9,6 +20,7 @@ def oxford_comma(array)
     last_word = array.pop.to_s
     array.join(", ") + ", and #{last_word}"
   end
+=end
 
 
 end
