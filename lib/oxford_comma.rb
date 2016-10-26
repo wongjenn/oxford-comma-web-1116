6dda5c7b11043e8a1b53a21afc9ac9a7f097f9b4
Jennifer Wong <wongjenn@yahoo.com>
2016-10-26 17:54:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  num = array.length
+
+  if num == 1
+    array.join("")
+  elsif num == 2
+    array.join(" and ")
+  elsif num > 2
+    last_word = array.pop.to_s
+    array.join(", ") + ", and #{last_word}"
+  end
+
 
 end
